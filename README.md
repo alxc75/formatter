afbf63c4010b18c1969a53aee8ca508a081673c2
xTellarin <alexis.cappe@me.com>
2024-09-25 16:16:09 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,49 @@
 # Formatter
+
 Clean and format your meeting notes in one click, optionally using templates.
 Uses the OpenAI API for the behind-the-scene magic. Get your key [here](https://platform.openai.com/api-keys).
 
-## To Do
-- [ ] Write an actual README
-- [ ] Simplify template creation and load
+## Get started
+
+1. Clone the repo or download the zip file.
+
+2. Install the dependencies with
+
+```python
+pip install openai streamlit
+```
+
+3. Open the terminal in the formatter directory and run `streamlit run app.py`
+
+4. If prompted, enter your OpenAI API key, press enter to save and refresh the page.
+
+That's it, you are now ready to go, enjoy!
+
+## Modes
+
+Formatter has 3 modes of operation:
+- **Clean**: The most basic mode, it just instructs the LLM to fix typos and grammatical mistakes. It will also try to un-abbreviate some words if it can.
+- **Format**: The star of the show, it instructs the LLM to format your notes according to a template you have created. See below for an example.
+- **Summarize**: Just as the name implies, it instructs the LLM to summarize the given text. No particular formatting is applied here.
+
+The system prompts are in the `app.py` main file if you'd like to tweak or extend them.
+
+## Setup the templates
+
+Formatter really shines once you've got your templates set up. You'll need to select the "Format" mode selector to use them.
+Templates are just markdown files (`.md`) where you create a skeleton of what your final note will look like.
+
+Typically, you will use markdown headings that the LLM will follow to structure your notes. I also strongly suggest to explain the contents of the subheading between brackets. Here's an example for a barebones note organizer:
+
+```markdown
+# Stakeholders
+[Who are the stakeholders?]
+
+# Context
+[What is the context of the meeting? What is the project being discussed? Put all the miscellaneous information here.]
+
+# Next Steps
+[What are the next steps? Assign them to a shareholder if possible.]
+```
+
+That is a basic example but it should give you a decent idea of what a template should look like. Since the LLM reads your template, then tries to format your notes according to the template, your imagination is the limit here.
---
+++ app.py
@@ -0,0 +1,95 @@
+import streamlit as st
+from openai import OpenAI
+import os
+
+st.set_page_config(page_title="Formatter", page_icon="🧠", layout="wide")
+
+st.title("Formatter")
+
+try:
+    api_key = os.environ["OPENAI_API_KEY"]
+except KeyError:
+    st.error("OpenAI API key not found. Please enter your key below, press Enter and refresh the page. Your key will be stored on your system.")
+    os_key = st.text_input("API Key", type="password")
+    if os_key:
+        os.environ["OPENAI_API_KEY"] = os_key
+    st.stop()
+
+client = OpenAI(api_key=api_key)
+
+st.write('Welcome to Formatter. Use GPT-4o to format your notes. Upload your text or file and select a template to get started.')
+
+mode = st.radio("Mode", ["Clean", "Format", "Summarize"], horizontal=True)
+
+
+if mode == 'Format':
+    templates = os.listdir("templates")
+    templates = [t.replace(".md", "").capitalize() for t in templates]
+    if len(templates) == 0:
+        templates = None
+        st.error("No templates found in the templates/ folder. Please refer to the README and add some templates to get started.")
+        st.stop()
+    elif len(templates) <= 5:
+        temp_list = [t.replace(".md", "").capitalize() for t in templates]
+        template = st.radio('Template', temp_list, horizontal=True, help="Edit your templates in the templates/ folder. See the README for more information.")
+    else:
+        template = st.selectbox("Template", templates, help="Edit your templates in the templates/ folder. See the README for more information.")
+
+    if template:
+        template_content = open(f"templates/{template.lower()}.md").read()
+
+
+file = st.file_uploader("Upload a file", type=["txt", "md"], help="Upload a text or markdown file")
+
+if file:
+    text = file.getvalue().decode("utf-8")
+else:
+    st.write("Or")
+    text = st.text_area("Enter your text here")
+
+
+
+# Instructions
+if mode == 'Clean':
+    clean_instructions = """
+    You are a note-taking assistant. Your task is to clean the text of any typos, error or grammatical mistakes. You MUST NOT format the text suing markdown or anything else. You must strive to keep the text as close to the original as possible. You are not allowed to add or remove any information from the text. Keep the words in their written language. The text can be in French, English or a mix of both. Here is the text you need to clean:
+    \n
+    """
+
+elif mode == 'Format':
+    format_instructions = f"""
+    You are a note-taking assistant. Your task is to clean the text of any typos, error or grammatical mistakes and format the text according to the given template. You may use the markdown syntax to format the text. You are not allowed to add or remove any information from the text. Keep the words in their written language. The text can be in French, English or a mix of both. Keep your response in the text's primary language (generally French).
+    Here the template you need to use:
+    {template_content}
+
+    Here is the text you need to format:
+    """
+
+else:
+    summarize_instructions = """
+    You are a note-taking assistant. Your task is to summarize the text. You are not allowed to add or remove any information from the text. Keep the words in their written language. The text can be in French, English or a mix of both. Keep your response in the text's primary language (generally French). Here is the text you need to summarize:
+    """
+
+
+def send_payload(text):
+    instructions = clean_instructions if mode == 'Clean' else format_instructions if mode == 'Format' else summarize_instructions
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": instructions},
+            {"role": "user", "content": text}
+        ]
+    )
+
+    return response.choices[0].message.content
+
+
+if text:
+    btn = st.button("Process")
+
+    if btn:
+        with st.spinner("Processing your text, please wait..."):
+            send_payload(text)
+            st.write(send_payload(text))
+
